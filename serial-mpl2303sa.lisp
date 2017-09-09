1c89a1c5999115a44ec71a4fbe9a868f5707156e
fireflower0 <fireflower0@hotmail.co.jp>
2017-09-09 14:06:12 +0900

    Create SERIAL-MPL2303SA

---
+++ serial-mpl2303sa.lisp
@@ -0,0 +1,30 @@
+;; Load packages
+(load "packages.lisp" :external-format :utf-8)
+
+(in-package :cl-cffi)
+
+;; Load wrapper API
+(load "libwiringPi.lisp" :external-format :utf-8)
+
+(defun main ()
+  (let (fd input-data)
+    ;; Serial Open
+    (setf fd (serialOpen "/dev/ttyAMA0" 115200))
+    (if (< fd 0)
+        (return-from main nil))
+
+    ;; Read text
+    (setf input-data (read-line))
+
+    ;; Send text
+    (loop :for char :across input-data
+       :do (serialPutchar fd (char-code char)))
+
+    ;; New line LF
+    (serialPutchar fd #X0A)
+
+    ;; Serial Close
+    (serialClose fd)))
+
+;; 実行！
+(main)
